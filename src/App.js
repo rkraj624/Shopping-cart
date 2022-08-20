aefc4aa1bd2f2c30bb8ada20858586002eaaec3b
Ravi Kishan Raj <ravikishan624@.com>
2022-08-20 16:16:22 +0530

    Cart Js completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useReducer } from 'react';
+import { cartReducer } from './reducers/cartReducers';
+import Products from './components/Products'
+import Cart from './components/Cart'
 
 function App() {
+  const [state, dispatch] = useReducer(cartReducer, {
+    products : [],
+    cart : [],
+  })
+
+  const fetchProducts = async () =>{
+    const {data} = await axios.get("https://dummyjson.com/products");
+    // console.log(data.products);
+    
+    dispatch({
+      type : "ADD_PRODUCT",
+      payload : data.products,
+    })
+   
+    console.log(state);
+    
+  }
+  useEffect(() => {
+   fetchProducts();
+  }, [])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display:"flex"}} >
+        <Products state={state} dispatch={dispatch} />
+        <Cart state={state} dispatch={dispatch} />
     </div>
   );
 }
